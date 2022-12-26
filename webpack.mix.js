8ce40acb55a4b25710ff4ecd9d15a900670a8ea1
Сергей <1@php-cat.com>
2022-12-26 23:42:54 +0500

    222

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,11 +20,14 @@ mix.mergeManifest();
 mix.copy('resources/img', 'public/img')
 mix.copy('resources/public', 'public')
 
+mix.copy('resources/to-storage-public', 'storage/app/public/')
+
 
 mix.js('resources/js/app.js', 'public/js')
     // .js('resources/phpcat/js/app.js', 'public/phpcat2/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
+
 
 // const mix2 = require('laravel-mix');
 // mix2.js('resources/phpcat/js/app.js', 'public/phpcat2/js')
